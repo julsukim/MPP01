94b6fe6d12aea62d1c15366816edde558a01346e
김준수 <pittuarez@naver.com>
2024-03-04 13:21:29 +0900

    feat: button styling

--- mpp01/src/components/Button.tsx
+++ mpp01/src/components/Button.tsx
@@ -1,40 +1,88 @@
 import React, {ButtonHTMLAttributes} from 'react';
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
+import {backgrounds, darken, lighten} from 'polished';
 
-const StyledButton = styled.button`
-  display: inline-flex;
+interface StyledButtonProps {
+  color: string,
+  size: string,
+  $outline: boolean
+}
+
+const colorStyles = css<StyledButtonProps>`
+  ${({ theme, color, $outline }) => {
+    const selected = theme.palette[color || ''];
+    return css`
+      background: ${selected};
+      &:hover {
+        background: ${lighten(0.1, selected)};
+      }
+      &:active {
+        background: ${darken(0.1, selected)};
+      }
+      ${$outline &&
+        css`
+          color: ${selected};
+          background: none;
+          border: 1px solid ${selected};
+          &:hover {
+            background: ${selected};
+            color: white;
+          }
+        `}
+    `;
+  }}
+`;
+
+const sizes: {[key:string]:{height:string; fontSize:string}} = {
+  large: {
+    height: '3rem',
+    fontSize: '1.25rem'
+  },
+  medium: {
+    height: '2.25rem',
+    fontSize: '1rem'
+  },
+  small: {
+    height: '1.75rem',
+    fontSize: '0.875rem'
+  },
+}
+
+const sizeStyles = css<StyledButtonProps>`
+  ${({ size }) => css`
+    height: ${sizes[size].height};
+    font-size: ${sizes[size].fontSize};
+  `}
+`;
+
+const StyledButton = styled.button<StyledButtonProps>`
   outline: none;
   border: none;
   border-radius: 4px;
   color: white;
-  font-weight: bold;
   cursor: pointer;
   padding-left: 1rem;
   padding-right: 1rem;
 
-  height: 2.5rem;
-  font-size: 1rem;
-
-  background: #b8b8b8;
-
-  &:hover {
-    background: #e5e5e5;
-  }
-
-  &:active {
-    background: #e1e1e1;
-  }
+  ${sizeStyles}
+  
+  ${colorStyles}
 
   & + & {
     margin-left: 1rem;
   }
 `;
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  color?: string,
+  size?: string,
+  $outline?: boolean
+}
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest}) => {
+const Button: React.FC<ButtonProps> = ({ children, color="blue", size="medium", $outline=false, ...rest}) => {
+  console.log(children, color, size, $outline)
   return (
-    <StyledButton {...rest}>
+    <StyledButton color={color} size={size} $outline={$outline} {...rest}>
       {children}
     </StyledButton>
   );
--- mpp01/src/pages/Home.tsx
+++ mpp01/src/pages/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Button from "../components/Button";
+import { ThemeProvider } from "styled-components";
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <Button>confirm</Button>
+      <ThemeProvider
+        theme={{
+          palette: {
+            blue: '#228be6',
+            gray: '#495057',
+            pink: '#f06595'
+          }
+        }}
+      >
+        <Button>confirm</Button>
+        <Button color="gray" size="small">confirm</Button>
+        <Button color="pink" size="large" $outline>confirm</Button>
+      </ThemeProvider>
     </div>
   );
 };
